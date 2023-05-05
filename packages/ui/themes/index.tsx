e5bd1700eb17bfab41746efdc05a5472cf9bc295
takadr <t4k4.cr@gmail.com>
2023-05-05 19:10:01 +0900

    Change initial color mode to dark

--- packages/ui/themes/index.tsx
+++ packages/ui/themes/index.tsx
@@ -2,6 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 
 const theme = extendTheme({
+    initialColorMode: 'dark',
     styles: {
         global: (props: any) => ({
             body: {
