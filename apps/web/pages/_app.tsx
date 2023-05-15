7d689d30efd5176eb74582c5a03fe3475e840efd
takadr <t4k4.cr@gmail.com>
2023-05-15 19:48:17 +0900

    Added apollo client

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,18 +1,22 @@
-import "ui/styles.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from '@chakra-ui/react';
-import theme from 'ui/themes';
 import { WagmiConfig } from 'wagmi';
+import { ApolloProvider } from '@apollo/client';
+import theme from 'ui/themes';
 import client from 'ui/connector'; 
+import apolloClient from 'ui/apollo/client';
 import { CurrentUserProvider } from 'ui/components/providers/CurrentUserProvider';
+import "ui/styles.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <WagmiConfig client={client}>
-        <CurrentUserProvider>
-          <Component {...pageProps} />
-        </CurrentUserProvider>
+        <ApolloProvider client={apolloClient}>
+          <CurrentUserProvider>
+            <Component {...pageProps} />
+          </CurrentUserProvider>
+        </ApolloProvider>
       </WagmiConfig>
     </ChakraProvider>
   );
---
+++ packages/ui/apollo/client.ts
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: `https://api.studio.thegraph.com/query/46860/${process.env.NEXT_PUBLIC_SUBGRAPH_SLUG}/${process.env.NEXT_PUBLIC_SUBGRAPH_VERSION}`,
+})
+
+export default client;
