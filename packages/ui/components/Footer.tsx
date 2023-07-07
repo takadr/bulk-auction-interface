8a4410ad6dfd5b5523d8c91f4d12f81a1233349b
takadr <t4k4.cr@gmail.com>
2023-07-07 21:03:47 +0900

    Apply format

--- packages/ui/components/Footer.tsx
+++ packages/ui/components/Footer.tsx
@@ -10,7 +10,7 @@ import {
   VStack,
   chakra,
 } from "@chakra-ui/react";
-import { AiFillGithub } from 'react-icons/ai';
+import { AiFillGithub } from "react-icons/ai";
 
 const Footer: FC = () => {
   return (
@@ -22,31 +22,27 @@ const Footer: FC = () => {
       opacity={0.975}
     >
       <Container maxW="container.2xl" px={{ base: 2, md: 4 }}>
-        <Flex
-          py="4"
-          justifyContent="center"
-          alignItems="center"
-        >
+        <Flex py="4" justifyContent="center" alignItems="center">
           <VStack>
-                <Link
-                href="https://github.com/DeFiGeek-Community/bulk-auction-interface"
-                target={'_blank'}
-                fontSize={'3xl'}
-                _hover={{ opacity: 0.8 }}
-                >
-                    <AiFillGithub />
-                </Link>
+            <Link
+              href="https://github.com/DeFiGeek-Community/bulk-auction-interface"
+              target={"_blank"}
+              fontSize={"3xl"}
+              _hover={{ opacity: 0.8 }}
+            >
+              <AiFillGithub />
+            </Link>
 
-                <HStack fontSize={'xs'} spacing={1}>
-                    <chakra.span>Current version: </chakra.span>
-                    <Link
-                    href={`https://github.com/takadr/bulk-auction-interface/commit/${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA}`}
-                    target={'_blank'}
-                    >
-                        {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA}
-                    </Link>
-                </HStack>
-            </VStack>
+            <HStack fontSize={"xs"} spacing={1}>
+              <chakra.span>Current version: </chakra.span>
+              <Link
+                href={`https://github.com/takadr/bulk-auction-interface/commit/${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA}`}
+                target={"_blank"}
+              >
+                {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA}
+              </Link>
+            </HStack>
+          </VStack>
         </Flex>
       </Container>
     </Box>
