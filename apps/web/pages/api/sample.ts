66e57b53cc6f8135d5e4c2694632c6300f85f6f0
takadr <92980001+takadr@users.noreply.github.com>
2023-12-28 10:49:58 +0900

    Update sample.ts

--- apps/web/pages/api/sample.ts
+++ apps/web/pages/api/sample.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   // console.log(process.env);
   console.log(process.env.IRON_SESSION_COOKIE_NAME);
-  console.log("---")
+  console.log("---");
   console.log(process.env.__NEXT_PRIVATE_STANDALONE_CONFIG);
   res.status(200).json({ test: "ok" });
 }
