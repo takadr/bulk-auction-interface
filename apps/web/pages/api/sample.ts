6f41dc36636b7695a2798306b546016afeae3a83
takadr <92980001+takadr@users.noreply.github.com>
2023-12-28 11:31:58 +0900

    Update sample.ts

--- apps/web/pages/api/sample.ts
+++ apps/web/pages/api/sample.ts
@@ -2,9 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   // console.log(process.env);
   console.log(process.env.IRON_SESSION_COOKIE_NAME);
-  console.log(process.env.AWS_ACCESS_KEY_ID);
-  console.log(process.env.AWS_SECRET_ACCESS_KEY);
-  console.log(process.env.AWS_REGION);
-  console.log(process.env.AWS_DYNAMO_TABLE_NAME);
+  console.log(process.env.IRON_SESSION_PASSWORD);
+  console.log(process.env._AWS_ACCESS_KEY_ID);
+  console.log(process.env._AWS_SECRET_ACCESS_KEY);
+  console.log(process.env._AWS_REGION);
+  console.log(process.env._AWS_DYNAMO_TABLE_NAME);
   res.status(200).json({ test: "ok" });
 }
