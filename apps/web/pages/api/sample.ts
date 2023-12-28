8b1850e5f0459979adfffab7c3d826a71a3f68c3
takadr <92980001+takadr@users.noreply.github.com>
2023-12-28 12:58:16 +0900

    Update sample.ts

--- apps/web/pages/api/sample.ts
+++ apps/web/pages/api/sample.ts
@@ -11,5 +11,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(process.env._AWS_SECRET_ACCESS_KEY);
   console.log(process.env._AWS_REGION);
   console.log(process.env._AWS_DYNAMO_TABLE_NAME);
+  console.log(process.env.NEXT_PUBLIC_INFURA_API_TOKEN);
+  console.log(process.env.NEXT_PUBLIC_CHAIN_ID);
   res.status(200).json({ test: "ok" });
 }
