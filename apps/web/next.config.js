f979519d6560a89ea942a1ff32dd2dda5e307753
takadr <92980001+takadr@users.noreply.github.com>
2023-12-28 12:00:07 +0900

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui", "lib"],
-  // env: {
-  //   IRON_SESSION_PASSWORD: process.env.IRON_SESSION_PASSWORD,
-  //   IRON_SESSION_COOKIE_NAME: process.env.IRON_SESSION_COOKIE_NAME,
-  //   AWS_ACCESS_KEY_ID: process.env._AWS_ACCESS_KEY_ID,
-  //   AWS_SECRET_ACCESS_KEY: process.env._AWS_SECRET_ACCESS_KEY,
-  //   AWS_REGION: process.env._AWS_REGION,
-  //   AWS_DYNAMO_TABLE_NAME: process.env._AWS_DYNAMO_TABLE_NAME,
-  // }
+  env: {
+    IRON_SESSION_PASSWORD: process.env.IRON_SESSION_PASSWORD,
+    IRON_SESSION_COOKIE_NAME: process.env.IRON_SESSION_COOKIE_NAME,
+    AWS_ACCESS_KEY_ID: process.env._AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env._AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env._AWS_REGION,
+    AWS_DYNAMO_TABLE_NAME: process.env._AWS_DYNAMO_TABLE_NAME,
+  }
 };
